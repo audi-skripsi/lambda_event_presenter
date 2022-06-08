96a20deb8186a79a8f754216e29ffff6e65c975a
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2022-06-08 15:36:28 +0800

    [feature] add router paths

--- cmd/webservice/router/paths.go
+++ cmd/webservice/router/paths.go
@@ -1,5 +1,8 @@
 package router
 
 const (
-	PingPath = "/v1/ping"
+	PingPath                  = "/v1/ping"
+	GetMicroservicesData      = "/v1/microservices"
+	GetMicroservicesAnalytics = "/v1/microservices/{id}/analytics"
+	GetMicroservicesEvents    = "/v1/microservices/{id}/events"
 )
