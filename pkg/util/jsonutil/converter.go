44a1b1e2c64a91bf22cdfe71f314596ddff3f5be
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2022-06-08 19:58:43 +0800

    [feature] add json converters

--- pkg/util/jsonutil/converter.go
+++ pkg/util/jsonutil/converter.go
@@ -23,3 +23,20 @@ func ConvertToObject(r *http.Request, i interface{}) (err error) {
 
 	return
 }
+
+func ConvertToJSONString(i interface{}) (jsonString string, err error) {
+	b, err := json.Marshal(i)
+	if err != nil {
+		err = errors.ErrBadRequest
+	}
+	jsonString = string(b)
+	return
+}
+
+func ConvertFromJSONSting(jsonString string, i interface{}) (err error) {
+	err = json.Unmarshal([]byte(jsonString), i)
+	if err != nil {
+		err = errors.ErrBadRequest
+	}
+	return
+}
