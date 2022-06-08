9ba4d40dd3055d4f3fb99d87ce59aacb6e4a2b72
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2022-06-08 15:33:56 +0800

    [feature] add webservice responses dto

---
+++ pkg/dto/webservice.go
@@ -0,0 +1,35 @@
+package dto
+
+type PublicMicroserviceData struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type PublicMicroserviceDataCount struct {
+	TotalEventData        int64 `json:"totalEventData"`
+	TotalInfoEventData    int64 `json:"totalInfoEventData"`
+	TotalWarnEventData    int64 `json:"totalWarnEventData"`
+	TotalErrorEventData   int64 `json:"totalErrorEventData"`
+	TotalUnknownEventData int64 `json:"totalUnknownEventData"`
+}
+
+type PublicEventData struct {
+	UID         string `json:"uid"`
+	Level       string `json:"level"`
+	Message     string `json:"message"`
+	ServiceName string `json:"serviceName"`
+	Timestamp   int64  `json:"timestamp"`
+}
+
+type PublicMicroservicesNameResponse struct {
+	Microservices []PublicMicroserviceData `json:"microservices"`
+}
+
+type PublicMicroserviceAnalyticsResponse struct {
+	EventDataCount PublicMicroserviceDataCount `json:"eventDataCount"`
+}
+
+type PublicMicroserviceEventSearchResponse struct {
+	TotalEventData int64           `json:"totalEventData"`
+	Events         PublicEventData `json:"events"`
+}
